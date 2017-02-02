3ab07c66184e6002414307477e784fb307175753
JH\JH <reach0328@gmail.com>
2017-02-02 18:11:27 +0900

    thread add

--- app/src/main/java/com/android/jh/musicplayer/PlayerActivity.java
+++ app/src/main/java/com/android/jh/musicplayer/PlayerActivity.java
@@ -5,12 +5,15 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
+import android.os.Handler;
 import java.util.ArrayList;
 
 public class PlayerActivity extends AppCompatActivity {
@@ -22,20 +25,43 @@ public class PlayerActivity extends AppCompatActivity {
 
     MediaPlayer player;
     SeekBar seekBar;
+    TextView txtDuration,txtCurrent;
 
     //플레이어 상태 플러그
     private static final int PLAY = 0;
     private static final int PAUSE = 1;
     private static final int STOP = 2;
 
-    private static int playStatus ;
+    private static int playStatus = STOP;
     //현재 음악 위치
     int position = 0;
 
+
+
+    // 핸들러 상태 플래그
+    public static final int PROGRESS_SET = 101;
+
+    // 핸들러
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            //super.handleMessage(msg);
+            switch(msg.what){
+                case PROGRESS_SET:
+                    if(player != null) {
+                        seekBar.setProgress(player.getCurrentPosition());
+                        txtCurrent.setText(player.getCurrentPosition());
+                    }
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        playStatus = STOP;
 
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
@@ -49,6 +75,8 @@ public class PlayerActivity extends AppCompatActivity {
         btnPlay.setOnClickListener(clickListener);
 
         seekBar= (SeekBar)findViewById(R.id.seekBar);
+        txtDuration = (TextView)findViewById(R.id.txtDuration);
+        txtCurrent = (TextView)findViewById(R.id.txtCurrent);
 
         // 0. 데이터 가져오기
         datas = DataLoader.get(this);
@@ -100,10 +128,27 @@ public class PlayerActivity extends AppCompatActivity {
                 player = MediaPlayer.create(this, musicUri);
 
                 player.setLooping(false);// 반복여부
+                //SeekBar 길이
+                seekBar.setMax(player.getDuration());
+                txtDuration.setText(player.getDuration()/1000+"Sec.");
+
                 player.start();
+
+                // subThread를 생성해서 미디어player의 현재 포지션 값을 seekbar를 변경해준다 매 1초마다
+
                 playStatus = PLAY;
                 //클릭시 버튼이미지 변경
                 btnPlay.setImageResource(android.R.drawable.ic_media_pause);
+                // sub thread 를 생성해서 mediaplayer 의 현재 포지션 값으로 seekbar 를 변경해준다. 매 1초마다
+                new Thread() {
+                    @Override
+                    public void run() {
+                        while (playStatus < STOP) {
+                            handler.sendEmptyMessage(PROGRESS_SET);
+                            try { Thread.sleep(1000); } catch (InterruptedException e) { }
+                        }
+                    }
+                }.start();
                 break;
             case PLAY : // 플레이중이면 멈춤
                 player.pause();
@@ -124,5 +169,14 @@ public class PlayerActivity extends AppCompatActivity {
 
     private void next() {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(player != null){
+            player.release(); // 사용이 끝나면 해제해야만 한다.
+        }
+        playStatus = STOP;
     }
 }
