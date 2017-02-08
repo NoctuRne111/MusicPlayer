dd3431e6d338f63cfd57a6d79bf36acd37c5a0ad
JH\JH <reach0328@gmail.com>
2017-02-08 17:42:53 +0900

    add pageTranformer

--- app/src/main/java/com/android/jh/musicplayer/PlayerActivity.java
+++ app/src/main/java/com/android/jh/musicplayer/PlayerActivity.java
@@ -70,6 +70,10 @@ public class PlayerActivity extends AppCompatActivity {
         viewPager.setAdapter( adapter );
         // 4. 뷰 페이저 리스너 연결
         viewPager.addOnPageChangeListener(viewPagerListener);
+        // 4-1. 페이지 트렌스 포머 연결
+        viewPager.setPageTransformer(false,pageTransformer);
+
+
         //특정 페이지 호출
         returnPage();
     }
@@ -287,5 +291,36 @@ public class PlayerActivity extends AppCompatActivity {
         public void onStartTrackingTouch(SeekBar seekBar) { }
         @Override
         public void onStopTrackingTouch(SeekBar seekBar) { }
+    };
+
+    // 페이지 트렌스포머
+    ViewPager.PageTransformer pageTransformer = new ViewPager.PageTransformer() {
+        @Override
+        public void transformPage(View page, float position) {
+
+            //현재 Page의 위치가 조금이라도 바뀔때마다 호출되는 메소드
+            //첫번째 파라미터 : 현재 존재하는 View 객체들 중에서 위치가 변경되고 있는 View들
+            //두번째 파라미터 : 각 View 들의 상대적 위치( 0.0 ~ 1.0 : 화면 하나의 백분율)
+
+            //           1.현재 보여지는 Page의 위치가 0.0
+            //           Page가 왼쪽으로 이동하면 값이 -됨. (완전 왼쪽으로 빠지면 -1.0)
+            //           Page가 오른쪽으로 이동하면 값이 +됨. (완전 오른쪽으로 빠지면 1.0)
+
+            //주의할 것은 현재 Page가 이동하면 동시에 옆에 있는 Page(View)도 이동함.
+            //첫번째와 마지막 Page 일때는 총 2개의 View가 메모리에 만들어져 잇음.
+            //나머지 Page가 보여질 때는 앞뒤로 2개의 View가 메모리에 만들어져 총 3개의 View가 instance 되어 있음.
+            //ViewPager 한번에 1장의 Page를 보여준다면 최대 View는 3개까지만 만들어지며
+            //나머지는 메모리에서 삭제됨.-리소스관리 차원.
+
+            //position 값이 왼쪽, 오른쪽 이동방향에 따라 음수와 양수가 나오므로 절대값 Math.abs()으로 계산
+            //position의 변동폭이 (-2.0 ~ +2.0) 사이이기에 부호 상관없이 (0.0~1.0)으로 변경폭 조절
+            //주석으로 수학적 연산을 설명하기에는 한계가 있으니 코드를 보고 잘 생각해 보시기 바랍니다.
+            float normalizedposition = Math.abs( 1 - Math.abs(position) );
+
+            page.setAlpha(normalizedposition);  //View의 투명도 조절
+            page.setScaleX(normalizedposition/2 + 0.5f); //View의 x축 크기조절
+            page.setScaleY(normalizedposition/2 + 0.5f); //View의 y축 크기조절
+            page.setRotationY(position * 80); //View의 Y축(세로축) 회전 각도
+        }
     };
 }
